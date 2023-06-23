6c9dc71b68663459706129e307baf351b906ade0
sebasian1662008 <137560002+sebasian1662008@users.noreply.github.com>
2023-06-23 17:35:41 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-for (let index = 0; index < 1; index++) {
-    basic.showString("hola")
-    while (true) {
-        music.play(music.stringPlayable("C D D F E C D F ", 500), music.PlaybackMode.UntilDone)
-    }
-    basic.showString("vienvenidos a la precentaccion de la radacion  ")
-    while (true) {
-        music.play(music.stringPlayable("C G D E D C F C ", 500), music.PlaybackMode.UntilDone)
-    }
+basic.showString("hola")
+while (true) {
+    music.play(music.stringPlayable("C D D F E C D F ", 500), music.PlaybackMode.UntilDone)
+    break;
+}
+basic.showString("vienvenidos a la precentaccion de la radacion  ")
+while (true) {
+    music.play(music.stringPlayable("C G D E D C F C ", 500), music.PlaybackMode.UntilDone)
+    break;
 }
